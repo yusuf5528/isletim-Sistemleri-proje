82f625689d0c34934868e47a096a1191702c346d
yusuf5528 <61833233+yusuf5528@users.noreply.github.com>
2021-11-30 15:18:53 +0300

    promt olusturuldu

---
+++ v0.1(sadece_prompt).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	char satir[80];
+	char konum[100];
+	char * parametreler[10][80];
+
+	//prompt gibi davranmasi icin sonsuz dongu 
+	while(1)
+	{
+		//komutların ve parametrelerin tutulacagi dizi
+		
+
+		getcwd(konum,100);
+		printf("%s/: sau > ",konum);
+		fgets(satir,100,stdin);
+
+
+
+			
+
+
+	}
+
+
+}
